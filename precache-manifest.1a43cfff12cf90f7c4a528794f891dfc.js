868d3e492b46f2b5a952ba24feb760a109831e24
Prechar <pxiong037@gmail.com>
2020-08-17 04:14:23 +0000

    Updates

---
+++ precache-manifest.1a43cfff12cf90f7c4a528794f891dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a9ba50f2fbc502b00b21de14f0774d",
+    "url": "/dad-jokes-react/index.html"
+  },
+  {
+    "revision": "21801eeea0fb83535ae4",
+    "url": "/dad-jokes-react/static/css/main.897ad9cc.chunk.css"
+  },
+  {
+    "revision": "6a025a8956c98abad1a5",
+    "url": "/dad-jokes-react/static/js/2.5b8b91fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-react/static/js/2.5b8b91fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21801eeea0fb83535ae4",
+    "url": "/dad-jokes-react/static/js/main.04cd7f23.chunk.js"
+  },
+  {
+    "revision": "bdc305e10dcf9a1a3d4e",
+    "url": "/dad-jokes-react/static/js/runtime-main.137fd87e.js"
+  }
+]);
